b0b005c1c352565a5fdaac0b30f9741dea2a677b
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-20 13:22:59 +0200

    Add project files.

---
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSecurity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    public record AutheticationData(string? UserName, string? Password);
+    public record UserData(int UserId, string UserName);
+
+    // api/authentication/token
+    [HttpPost("token")]
+    public ActionResult<string> Authenticate([FromBody] AutheticationData data) 
+    {
+        var user = ValidateCredentials(data);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        return Ok(user);
+    }
+
+    private string GenerateToken(UserData userData) 
+    {
+    
+    }
+
+    private UserData? ValidateCredentials(AutheticationData data)
+    {
+        // THIS IS NOT PRODUCTION CODE - THIS IS ONLY FOR DEMONSTRATION PURPOSES - DO NO USE THIS IN REAL LIFE
+        
+        if (CompareValues(data.UserName, "tcorey") &&
+            CompareValues(data.Password, "Test123"))
+        { 
+            return new UserData(1, data.UserName!);
+        }
+
+        if (CompareValues(data.UserName, "sstorm") &&
+            CompareValues(data.Password, "Test123"))
+        {
+            return new UserData(2, data.UserName!);
+        }
+
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
+        if (actual is not null)
+        {
+            return actual.Equals(expected);
+        }
+
+        return false;
+    }
+}
---
+++ ApiSecurity/Controllers/UsersControllers.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiSecurity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersControllers : ControllerBase
+{
+    private IConfiguration _iconfig;
+    public UsersControllers(IConfiguration iconfig)
+    {
+        _iconfig = iconfig;
+    }
+    // GET: api/<UsersControllers>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<UsersControllers>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return _iconfig.GetConnectionString("DefaultConnection");
+    }
+
+    // POST api/<UsersControllers>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersControllers>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersControllers>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
